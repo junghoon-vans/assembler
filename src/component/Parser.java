5643465e24a3b0d63045a7a6ff85873d05ac9e65
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-04-11 17:59:37 +0900

    feat(component): Add parser class

---
+++ src/component/Parser.java
@@ -0,0 +1,13 @@
+package component;
+
+public class Parser {
+  private Lex lex;
+
+  public Parser(Lex lex) {
+    this.lex = lex;
+  }
+
+  public void parse() {
+    // TODO Auto-generated method stub
+  }
+}
