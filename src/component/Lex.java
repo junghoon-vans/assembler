14f3064061246890e5d9a83c5139383a38767a2b
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-04-11 17:53:47 +0900

    feat(component): Add lexical analyzer class

---
+++ src/component/Lex.java
@@ -0,0 +1,26 @@
+package component;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Lex {
+  private Scanner scanner;
+
+  public Lex(String filepath) {
+    File file = new File(filepath);
+    try {
+      scanner = new Scanner(file);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String getToken() {
+    try {
+      return scanner.next();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
