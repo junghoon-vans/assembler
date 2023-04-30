785ac8df4b3d291f7a83143fbcb1b7515f6c232f
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-04-30 21:11:38 +0900

    feat: Add program execution option

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import parse.symbol.SymbolTable;
 import parse.tree.Statement;
 import util.CodeGenerator;
@@ -7,20 +8,52 @@ import util.Lex;
 import util.Parser;
 
 public class Main {
-    public static void main(String[] args) {
-        Lex lex = new Lex("resources/program.asm");
-        SymbolTable symbolTable = new SymbolTable();
-        List<Statement> statements = new ArrayList<>();
 
-        Parser parser = new Parser(lex, symbolTable, statements);
+    static Lex lex;
+    static SymbolTable symbolTable;
+    static List<Statement> statements;
+    static Parser parser;
+
+    public static void main(String[] args) {
+        lex = new Lex("resources/program.asm");
+        symbolTable = new SymbolTable();
+        statements = new ArrayList<>();
+
+        parser = new Parser(lex, symbolTable, statements);
         parser.parse();
         lex.finalize();
 
-        CodeGenerator codeGenerator = new CodeGenerator("resources/program.exe");
-        codeGenerator.generate(symbolTable, statements);
+        String option = selectOption();
+        execute(option);
+    }
 
-        lex.printTokens();
-        symbolTable.print();
-        parser.print();
+    private static String selectOption() {
+        System.out.println("1 - Print tokens of lexical analyzer");
+        System.out.println("2 - Print symbol table and parse tree");
+        System.out.println("3 - Generate code");
+        System.out.print("Select an option: ");
+
+        Scanner sc = new Scanner(System.in);
+        String option = sc.nextLine();
+        sc.close();
+
+        return option;
+    }
+
+    private static void execute(String option) {
+        System.out.println();
+
+        if (option.equals("1")) {
+            lex.printTokens();
+        }
+        if (option.equals("2")) {
+            symbolTable.print();
+            parser.print();
+        }
+        if (option.equals("3")) {
+            CodeGenerator codeGenerator = new CodeGenerator("resources/program.exe");
+            codeGenerator.generate(symbolTable, statements);
+            System.out.println("Code generated successfully!");
+        }
     }
 }
