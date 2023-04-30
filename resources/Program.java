a26fd86ba78ed94d28df47e9793308aa939e9b8e
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-04-30 20:40:06 +0900

    chore: Add high level code

---
+++ resources/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+  public static void main(String[] args) {
+    int sum = 0;
+    int count = 10;
+
+    for (int i=0; i<count; i++) {
+      sum = sum + 1;
+    }
+
+    System.out.println(sum);
+  }
+}
