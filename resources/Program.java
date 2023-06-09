a3d2a9fabca35b6ba13abc0467bacef4d96625cd
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-06-09 21:15:27 +0900

    chore(resources): Update program source code

--- resources/Program.java
+++ resources/Program.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Program {
   public static void main(String[] args) {
-    int sum = 0;
-    int count = 10;
 
-    for (int i=0; i<count; i++) {
-      sum = sum + 1;
+    Scanner scanner = new Scanner(System.in);
+    int count = scanner.nextInt();
+
+    for (int i = 0; i < count; i++) {
+      Student student = new Student();
+      student.average();
     }
+  }
+}
 
-    System.out.println(sum);
+class Student {
+  private static int kor = 60;
+  private static int eng = 70;
+
+  private int sum() {
+    return this.kor + this.eng;
+  }
+
+  public void average() {
+    int result = sum() / 2;
+    System.out.println(result);
   }
 }
