3565bbeed395e3256bd8089f3fe6ea9da181f55a
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-06-11 03:52:04 +0900

    refactor(Operator): Update operator sets

--- src/instruction/Operator.java
+++ src/instruction/Operator.java
@@ -2,24 +2,26 @@ package instruction;
 
 public enum Operator {
   HALT,
-  LDA,
-  STA,
-  ADD,
-  AND,
-  JMP,
-  BZ,
-  NOT,
-  MOV,
+  LOADA,
+  LOADC,
+  STORE,
+  ADDA,
+  ADDC,
   SUB,
-  GTJ,
+  DIV,
+  JUMP,
+  BZ,
+  PUSH,
+  POP,
+  OUT,
   ;
 
   public static String hexCode(String operator) {
-    return String.format("%02X", Operator.valueOf(operator).ordinal());
+    return String.format("%02X", Operator.valueOf(operator.toUpperCase()).ordinal());
   }
 
   public static String binaryCode(String operator) {
-    return String.format("%04d", Operator.valueOf(operator).ordinal());
+    return String.format("%04d", Operator.valueOf(operator.toUpperCase()).ordinal());
   }
 }
 
